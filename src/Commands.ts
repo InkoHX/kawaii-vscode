8eba08d80c9d3d004cbfcc8e3ed2a22b0fb9ad87
InkoHX <inkohx@gmail.com>
2019-07-07 23:15:31 +0900

    Add fixgirl command.

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,12 +1,14 @@
 import { commands, ExtensionContext } from "vscode";
 import Command from "./command/base/Command";
 import CatGirl from "./command/picture/CatgirlCommand";
+import FoxGirl from "./command/picture/FoxgirlCommand";
 import CatGirlGif from "./command/picture/gif/CatgirlGifCommand";
 
 function getCommandList(context: ExtensionContext): Command[] {
   return [
     new CatGirl(context, "catgirl"),
     new CatGirlGif(context, "catgirl-gif"),
+    new FoxGirl(context, "foxgirl"),
   ];
 }
 
