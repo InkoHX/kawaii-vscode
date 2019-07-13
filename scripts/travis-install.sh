dc81e75dd70d3663f7e5df7cb98144c0eb469487
InkoHX <inkohx@gmail.com>
2019-07-13 17:11:01 +0900

    Fix scripts.

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-YELLOW="\033[1;33m"
-GREEN="\033[0;32m"
+YELLOW="\033[38;5;11m"
+GREEN="\033[38;5;10m"
 NOCOLOR="\033[0m"
 
-echo "${YELLOW}[UPDATE] ${NOCOLOR}Updating Yarn"
+echo -e "${YELLOW}[UPDATE] ${NOCOLOR}Updating Yarn"
 curl -o- -L https://yarnpkg.com/install.sh | bash
 export PATH=$HOME/.yarn/bin:$PATH
-echo "${YELLOW}[UPDATE] ${NOCOLOR}Yarn has been updated."
+echo -e "${YELLOW}[UPDATE] ${NOCOLOR}Yarn has been updated."
 
 yarn --version
--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-YELLOW="\033[1;33m"
-GREEN="\033[0;32m"
+YELLOW="\033[38;5;11m"
+GREEN="\033[38;5;10m"
 NOCOLOR="\033[0m"
 
 dir="dist"
 
-echo "${YELLOW}[TEST] ${NOCOLOR}checks TypeScript code."
+echo -e "${YELLOW}[TEST] ${NOCOLOR}checks TypeScript code."
 yarn run lint
-echo "${GREEN}[TEST] Passing"
+echo -e "${GREEN}[TEST] Passing"
 
 if [ -d $dir ]; then
-  echo "${YELLOW}[INFO] ${NOCOLOR}Removing old output folder..."
+  echo -e "${YELLOW}[INFO] ${NOCOLOR}Removing old output folder..."
   rm -rf $dir
-  echo "${GREEN}[INFO] ${NOCOLOR}Done."
+  echo -e "${GREEN}[INFO] ${NOCOLOR}Done."
 fi
 
-echo "${YELLOW}[TEST] ${NOCOLOR}compile"
+echo -e "${YELLOW}[TEST] ${NOCOLOR}compile"
 yarn run build
-echo "${GREEN}[TEST] Passing"
+echo -e "${GREEN}[TEST] Passing"
 
 echo "${YELLOW}[INFO] ${NOCOLOR}Removing output folder..."
 rm -rf $dir
