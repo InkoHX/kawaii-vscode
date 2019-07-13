1819a069f8a3861e58a0595d2de642e044115439
InkoHX <inkohx@gmail.com>
2019-07-13 17:34:58 +0900

    color fix

--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -2,24 +2,25 @@
 
 YELLOW="\033[33m"
 GREEN="\033[32m"
+CYAN="\033[36m"
 NOCOLOR="\033[0m"
 
 dir="dist"
 
 printf "${YELLOW}[TEST] ${NOCOLOR}checks TypeScript code.\n\n"
 yarn run lint
-printf "\n${GREEN}[TEST] Passing\n\n"
+printf "\n${GREEN}[TEST] ${CYAN}Passing\n\n"
 
 if [ -d $dir ]; then
   printf "${YELLOW}[INFO] ${NOCOLOR}Removing old output folder...\n"
   rm -rf $dir
-  printf "${GREEN}[INFO] ${NOCOLOR}Done.\n"
+  printf "${GREEN}[INFO] ${CYAN}Done.\n"
 fi
 
 printf "${YELLOW}[TEST] ${NOCOLOR}compile\n\n"
 yarn run build
-printf "\n${GREEN}[TEST] Passing\n"
+printf "\n${GREEN}[TEST] ${CYAN}Passing\n"
 
 printf "${YELLOW}[INFO] ${NOCOLOR}Removing output folder...\n"
 rm -rf $dir
-printf "${GREEN}[INFO] ${NOCOLOR}Done.\n"
+printf "${GREEN}[INFO] ${CYAN}Done.\n"
