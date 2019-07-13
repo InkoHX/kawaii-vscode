99f20fb4be4853107c44b28329b4ba78fb51af95
InkoHX <inkohx@gmail.com>
2019-07-13 17:31:32 +0900

    minor fix.

--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-YELLOW="\033[38;5;11m"
-GREEN="\033[38;5;10m"
+YELLOW="\033[33m"
+GREEN="\033[32m"
 NOCOLOR="\033[0m"
 
 dir="dist"
 
-printf "${YELLOW}[TEST] ${NOCOLOR}checks TypeScript code.\n"
+printf "${YELLOW}[TEST] ${NOCOLOR}checks TypeScript code.\n\n"
 yarn run lint
-printf "${GREEN}[TEST] Passing\n"
+printf "\n${GREEN}[TEST] Passing\n\n"
 
 if [ -d $dir ]; then
   printf "${YELLOW}[INFO] ${NOCOLOR}Removing old output folder...\n"
@@ -16,9 +16,9 @@ if [ -d $dir ]; then
   printf "${GREEN}[INFO] ${NOCOLOR}Done.\n"
 fi
 
-printf "${YELLOW}[TEST] ${NOCOLOR}compile\n"
+printf "${YELLOW}[TEST] ${NOCOLOR}compile\n\n"
 yarn run build
-printf "${GREEN}[TEST] Passing\n"
+printf "\n${GREEN}[TEST] Passing\n"
 
 printf "${YELLOW}[INFO] ${NOCOLOR}Removing output folder...\n"
 rm -rf $dir
