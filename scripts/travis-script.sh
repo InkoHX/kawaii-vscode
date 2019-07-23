2e3cba1e6bca0ccb77dea47f15eb6f52bcf8f1db
InkoHX <inkohx@gmail.com>
2019-07-23 16:06:58 +0900

    Fixes.

--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -28,11 +28,13 @@ eslint_version=`npx eslint --version` || (
   exit 1;
 )
 
-printf "${GREEN}[INFO] ${NOCOLOR}=== Kawaii VS Code - Test ===\n"
-printf "${GREEN}[INFO] ${NOCOLOR}Node: ${node_version}\n"
-printf "${GREEN}[INFO] ${NOCOLOR}Yarn: ${yarn_version}\n"
-printf "${GREEN}[INFO] ${NOCOLOR}TypeScript: ${typescript_version/"Version "/"v"}\n"
-printf "${GREEN}[INFO] ${NOCOLOR}ESLint: ${eslint_version}\n\n"
+printf "${GREEN}[INFO] ${CYAN}   === Kawaii VS Code - Test ===\n"
+printf "${GREEN}[INFO] ${NOCOLOR}Node: ${CYAN}${node_version}\n"
+printf "${GREEN}[INFO] ${NOCOLOR}Yarn: ${CYAN}${yarn_version}\n"
+printf "${GREEN}[INFO] ${NOCOLOR}TypeScript: ${CYAN}${typescript_version/"Version "/"v"}\n"
+printf "${GREEN}[INFO] ${NOCOLOR}ESLint: ${CYAN}${eslint_version}\n\n"
+
+sleep 3
 
 dir="dist"
 
