94dc6c4fb898bfa250a8e2c28a428e19bb80ba84
InkoHX <inkohx@gmail.com>
2019-07-23 15:49:09 +0900

    Update travis.script

--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -2,10 +2,37 @@
 
 set -eu
 
+RED="\033[31m"
 YELLOW="\033[33m"
 GREEN="\033[32m"
 CYAN="\033[36m"
 NOCOLOR="\033[0m"
+
+yarn_version="yarn --version" || (
+  printf "${RED}[ERROR] Yarn command does not exist."
+  exit 1;
+)
+
+node_version="node --version" || (
+  printf "${RED}[ERROR] Node command does not exist."
+  exit 1;
+)
+
+typescript_version="npx tsc --version" || (
+  printf "${RED}[ERROR] npx command does not exist"
+  exit 1;
+)
+
+eslint_version="npx eslint --version" || (
+  printf "${RED}[ERROR] npx command does not exist"
+  exit 1;
+)
+
+printf "${GREEN}[INFO] === Kawaii VS Code - Test ==="
+printf "${GREEN}[INFO] Node: ${node_version}"
+printf "${GREEN}[INFO] Yarn: ${yarn_version}"
+printf "${GREEN}[INFO] TypeScript: ${typescript_version/"Version "/"v"}"
+printf "${GREEN}[INFO] ESLint: ${eslint_version}"
 
 dir="dist"
 
