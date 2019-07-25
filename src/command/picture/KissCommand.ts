62b3f3ca17e80769449e8fcec35e40afb10374cb
InkoHX <inkohx@gmail.com>
2019-07-25 19:13:58 +0900

    Update KissCommand.ts (#9)

--- src/command/picture/KissCommand.ts
+++ src/command/picture/KissCommand.ts
@@ -1,11 +1,19 @@
-import { window } from 'vscode'
 import PictureCommand from '../base/PictureCommand'
 
 export default class extends PictureCommand {
   public async run (): Promise<void> {
-    const data = await this.v2.getSfwBody('kiss')
-    if (data.msg) window.showErrorMessage(data.msg === '404' ? 'API not found.' : data.msg)
-    if (!data.url) window.showErrorMessage('Image URL not found.')
-    else this.createWebviewPanel(data.url)
+    if (this.getVersion() === 'v3') {
+      const data = await this.v3.getSfwGif("kiss").then((res) => res.data)
+      if (data.status.success === false) {
+        const message = data.status.message ? data.status.message : 'Request failed.'
+        this.window.showErrorMessage(`Code ${data.status.code}: ${message}`)
+      } else if (!data.response.url) this.window.showErrorMessage('Image URL not found')
+      else this.createWebviewPanel(data.response.url)
+    } else {
+      const data = await this.v2.getSfwBody('kiss')
+      if (data.msg) this.window.showErrorMessage(data.msg === '404' ? 'API not found.' : data.msg)
+      else if (!data.url) this.window.showErrorMessage('Image URL not found.')
+      else this.createWebviewPanel(data.url)
+    }
   }
 }
