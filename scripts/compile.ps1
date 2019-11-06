efbb74d6efacfbdc6abdc800af2d6a485aba25db
InkoHX <inkohx@gmail.com>
2019-11-06 14:53:28 +0900

    Remove scripts.

--- scripts/compile.ps1
+++
@@ -1,9 +0,0 @@
-$outputfolder = "dist"
-
-if (Test-Path $outputfolder) {
-  Write-Output "Removing old output folder..."
-  Remove-Item $outputfolder -Recurse
-  Write-Output "Done."
-}
-
-yarn.cmd run compile
