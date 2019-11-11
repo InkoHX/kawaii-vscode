31df33f0c08d6ac2cf380234b23240c8c85d1dc3
InkoHX <inkohx@gmail.com>
2019-11-11 12:08:11 +0900

    Align center

--- src/command/PictureCommand.ts
+++ src/command/PictureCommand.ts
@@ -28,14 +28,15 @@ export default abstract class PictureCommand extends Command {
       <html lang="en">
       <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta
           http-equiv="Content-Security-Policy"
           content="default-src 'none'; img-src ${webview.cspSource} https:;"
         />
       </head>
       <body>
-        <img style="position:absolute;top:0;bottom:0;right:0;left:0;margin:auto;" src="${url}" />
+        <div align="center">
+          <img src="${url}" />
+        </div>
       </body>
       </html>`
   }
